1b8db4d40faf0adcbaf690b06c43c6b1cc1390cd
EvgeniyGrekov <evgeniygrekov@mail.ru>
2025-12-11 13:32:16 +0300

    Rename map-object ui

---
+++ src/app/ui/map-object-card/map-object-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-map-object-card',
+  imports: [],
+  templateUrl: './map-object-card.component.html',
+  styleUrl: './map-object-card.component.css',
+})
+export class mapObjectCardComponent {
+
+}
---
+++ src/app/ui/map-object-list/map-object-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-map-object-list',
+  imports: [],
+  templateUrl: './map-object-list.component.html',
+  styleUrl: './map-object-list.component.css',
+})
+export class MapObjectListComponent {
+
+}
