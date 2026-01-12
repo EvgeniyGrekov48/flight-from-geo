9e6d0df61961e876e39bac2e560e66244da7fc16
EvgeniyGrekov <evgeniygrekov@mail.ru>
2026-01-12 12:02:18 +0300

    Add style for object-detail component

--- src/app/features/object-detail/object-detail.component.ts
+++ src/app/features/object-detail/object-detail.component.ts
@@ -1,12 +1,21 @@
 import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DecimalPipe } from '@angular/common';
 import { RoutingStore } from '../../core/stores/routing.store';
 import { MapObjectService } from '../../core/services/map-object.service';
+import { TuiTitle, TuiButton, TuiHint } from '@taiga-ui/core';
+import { TuiBadge } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-object-detail',
-  imports: [],
+  imports: [
+    DecimalPipe,
+    TuiTitle,
+    TuiBadge,
+    TuiButton,
+    TuiHint,
+  ],
   templateUrl: './object-detail.component.html',
   styleUrl: './object-detail.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,12 +23,12 @@ import { MapObjectService } from '../../core/services/map-object.service';
 export class ObjectDetailComponent implements OnInit {
   private readonly _route = inject(ActivatedRoute);
   private readonly _destroyRef = inject(DestroyRef);
-
   private readonly _mapObjectService = inject(MapObjectService);
   private readonly _routingStore = inject(RoutingStore);
-
-  protected readonly object = computed(() => this._mapObjectService.getObjects()
-    .find(obj => obj.id === this._routingStore.getOpenedObjectId())
+  
+  protected readonly object = computed(() => 
+    this._mapObjectService.getObjects()
+      .find(obj => obj.id === this._routingStore.getOpenedObjectId())
   );
 
   ngOnInit(): void {
@@ -32,6 +41,6 @@ export class ObjectDetailComponent implements OnInit {
   }
 
   protected closeDetail(): void {
-    this._routingStore.routerNavigateToList()
+    this._routingStore.routerNavigateToList();
   }
 }
