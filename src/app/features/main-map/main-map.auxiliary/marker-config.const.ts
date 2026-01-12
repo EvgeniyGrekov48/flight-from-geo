aaf1075b7c23a38aabe2403390ba5e2c648c6e95
EvgeniyGrekov <evgeniygrekov@mail.ru>
2026-01-12 15:11:36 +0300

    Add badge to object-detail

--- src/app/features/main-map/main-map.auxiliary/marker-config.const.ts
+++ src/app/features/main-map/main-map.auxiliary/marker-config.const.ts
@@ -4,10 +4,16 @@ export const MARKER_RADIUS = 8;
 export const MARKER_BORDER = 2;
 export const SELECTED_MULTIPLIER = 2;
 
-export const MARKER_COLORS = {
+export const OBJECT_TYPE_COLOR = {
   [EnumMapObject.PARAGLIDING]: 'hsl(210, 80%, 40%)',
-  [EnumMapObject.THERMAL]: 'hsl(0, 80%, 40%)',
+  [EnumMapObject.THERMAL]: 'hsl(0, 80%, 50%)',
   [EnumMapObject.USER]: 'hsl(140, 80%, 35%)'
+} as const;
+
+export const OBJECT_TYPE_ALIAS = {
+  [EnumMapObject.PARAGLIDING]: 'Полет',
+  [EnumMapObject.THERMAL]: 'Термик',
+  [EnumMapObject.USER]: 'Пользователь'
 } as const;
 
 export const MARKER_Z_INDEX = {
--- src/app/features/object-detail/object-detail.component.ts
+++ src/app/features/object-detail/object-detail.component.ts
@@ -6,6 +6,8 @@ import { RoutingStore } from '../../core/stores/routing.store';
 import { MapObjectService } from '../../core/services/map-object.service';
 import { TuiTitle, TuiButton, TuiHint } from '@taiga-ui/core';
 import { TuiBadge } from '@taiga-ui/kit';
+import { OBJECT_TYPE_ALIAS, OBJECT_TYPE_COLOR } from '../main-map/main-map.auxiliary/marker-config.const';
+import { EnumMapObject } from '../../core/types/types';
 
 @Component({
   selector: 'app-object-detail',
@@ -15,7 +17,7 @@ import { TuiBadge } from '@taiga-ui/kit';
     TuiBadge,
     TuiButton,
     TuiHint,
-  ],
+],
   templateUrl: './object-detail.component.html',
   styleUrl: './object-detail.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -25,7 +27,7 @@ export class ObjectDetailComponent implements OnInit {
   private readonly _destroyRef = inject(DestroyRef);
   private readonly _mapObjectService = inject(MapObjectService);
   private readonly _routingStore = inject(RoutingStore);
-  
+
   protected readonly object = computed(() => 
     this._mapObjectService.getObjects()
       .find(obj => obj.id === this._routingStore.getOpenedObjectId())
@@ -42,5 +44,13 @@ export class ObjectDetailComponent implements OnInit {
 
   protected closeDetail(): void {
     this._routingStore.routerNavigateToList();
+  }
+
+  protected getObjectTypeColor(type: EnumMapObject): string {
+    return OBJECT_TYPE_COLOR[type]
+  }
+
+  protected getObjectTypeAlias(type: EnumMapObject): string {
+    return OBJECT_TYPE_ALIAS[type]
   }
 }
